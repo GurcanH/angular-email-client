5516c10766419de6c9396ef55892503ed04d719d
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-31 14:14:34 +1100

    280. Understanding Async Validators

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -10,7 +10,9 @@ export class UniqueUsername implements AsyncValidator {
 
   validate = (control: FormControl) => {
     const { value } = control;
-    console.log(this.http);
-    return null;
+
+    return this.http.post<any>('https://api.angular-email.com/auth/username', {
+      username: value,
+    });
   };
 }
